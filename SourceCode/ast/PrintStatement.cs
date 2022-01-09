bf6e83c4f62ab7dd5ccc685fdf76553e905c027b
Fedukdas <68533770+Fedukdas@users.noreply.github.com>
2022-01-09 17:18:24 +0300

    Stacks added

---
+++ SourceCode/ast/PrintStatement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class PrintStatement : Statement
+    {
+        private Expression expression;
+
+        public PrintStatement(Expression expression)
+        {
+            this.expression = expression;
+        }
+
+        public void execute()
+        {
+            Console.Write(expression.eval().asString());
+        }
+
+        public override string ToString()
+        {
+            return "print " + expression;
+        }
+    }
+}
