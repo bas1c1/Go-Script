aee910d6be8a774e69d502c1bce3e7d2ec3b3953
Fedukdas <68533770+Fedukdas@users.noreply.github.com>
2022-01-16 17:58:56 +0300

    Version2

---
+++ SourceCode/ast/lib/ImportStatement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class ImportStatement : Statement
+    {
+        private Expression expression;
+
+        public ImportStatement(Expression expression)
+        {
+            this.expression = expression;
+        }
+
+        public void execute()
+        {
+            string input = File.ReadAllText(expression.ToString());
+            List<Token> tokens = new Lexer(input).tokenize();
+            Statement program = new Parser(tokens).parse();
+            program.execute();
+        }
+    }
+}
---
+++ SourceCode/ast/lib/UserValue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    class UserValue : Value
+    {
+        private object value;
+
+        public UserValue(object value)
+        {
+            this.value = value;
+        }
+
+        public double asDouble()
+        {
+            try
+            {
+                return double.Parse(value.ToString());
+            }
+
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public string asString()
+        {
+            return value.ToString();
+        }
+
+        public int asNumber()
+        {
+            try
+            {
+                return int.Parse(value.ToString());
+            }
+
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public char asChar()
+        {
+            try
+            {
+                return char.Parse(value.ToString());
+            }
+
+            catch
+            {
+                return ' ';
+            }
+        }
+
+        public TokenType type()
+        {
+            return TokenType.OBJ;
+        }
+    }
+}
