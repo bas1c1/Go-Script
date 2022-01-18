f25c4f2655842ad4a21a07ab53d9f0f30cfe7ddf
mixu <68533770+mixu-maker@users.noreply.github.com>
2022-01-18 22:09:59 +0300

    Version 4 (cs library)

---
+++ SourceCode/ast/AssignmentStatement.cs
@@ -0,0 +1,33 @@
+﻿using OwnLang.ast.lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast
+{
+    class AssignmentStatement : Statement
+    {
+        private string variable;
+        private Expression expression;
+
+        public AssignmentStatement(string variable, Expression expression)
+        {
+            this.variable = variable;
+            this.expression = expression;
+        }
+
+        public void execute()
+        {
+            // expression.eval()
+            Value result = expression.eval();
+            Variables.set(variable, result);
+        }
+
+        public override string ToString()
+        {
+            return string.Format((variable.ToString() + " = " + expression.ToString()), variable, expression);
+        }
+    }
+}
---
+++ SourceCode/ast/FunctionDefine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class FunctionDefine : Statement
+    {
+        private string name;
+        private List<string> argNames;
+        private Statement body;
+
+        public FunctionDefine(string name, List<string> argNames, Statement body)
+        {
+            this.name = name;
+            this.argNames = argNames;
+            this.body = body;
+        }
+
+        public void execute()
+        {
+            Functions.set(name, new UserDefineFunction(argNames, body));
+        }
+
+        public override string ToString()
+        {
+            return "def (" + argNames.ToString() + ") " + body.ToString();
+        }
+    }
+}
