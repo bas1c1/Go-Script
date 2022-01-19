eb8f1739ed5ff54fca3c38602dcdc230d80c6f20
mixu <68533770+mixu-maker@users.noreply.github.com>
2022-01-19 11:55:30 +0300

    Version 4 (sublime syntax update)

---
+++ SourceCode/ast/lib/Value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public interface Value
+    {
+        double asDouble();
+        string asString();
+        int asNumber();
+        char asChar();
+        TokenType type();
+    }
+}
---
+++ SourceCode/ownLang/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing;
+using System.CodeDom.Compiler;
+
+namespace OwnLang.ast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Onion";
+            execute(args[0].ToString());
+        }
+
+        public static void execute(string file)
+        {
+            string input = File.ReadAllText(file);
+            List<Token> tokens = new Lexer(input).tokenize();
+            //foreach (Token tok in tokens)
+            //{
+            //    Console.WriteLine(tok);
+            //}
+            Statement program = new Parser(tokens).parse();
+            program.execute();
+            Console.WriteLine();
+        }
+    }
+}
