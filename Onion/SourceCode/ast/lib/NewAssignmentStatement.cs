3d480282c284f34594eed30956eee7b49edeed59
mixu <68533770+mixu-maker@users.noreply.github.com>
2022-01-24 19:49:49 +0300

    Version 5 (added OnionCORE netstandard2.0)

---
+++ Onion/SourceCode/ast/lib/NewAssignmentStatement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    class NewAssignmentStatement : Statement
+    {
+        private string variable;
+        private Expression expression;
+        private string operate;
+
+        public NewAssignmentStatement(string variable, Expression expression, string operate)
+        {
+            this.variable = variable;
+            this.expression = expression;
+            this.operate = operate;
+        }
+
+        public void execute()
+        {
+            // expression.eval()
+            double var = Variables.get(variable).asDouble();
+            Value result = new NumberValue(0);
+            if (operate == "+")
+            {
+                result = new NumberValue(var += expression.eval().asDouble());
+            }
+            if (operate == "-")
+            {
+                result = new NumberValue(var -= expression.eval().asDouble());
+            }
+            if (operate == "*")
+            {
+                result = new NumberValue(var *= expression.eval().asDouble());
+            }
+            if (operate == "/")
+            {
+                result = new NumberValue(var /= expression.eval().asDouble());
+            }
+            Variables.set(variable, result);
+        }
+
+        public override string ToString()
+        {
+            return string.Format((variable.ToString() + " = " + expression.ToString()), variable, expression);
+        }
+    }
+}
