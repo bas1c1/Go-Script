704fde4a44027c772fdb9f7374fcfb1675448544
mixu <68533770+mixu-maker@users.noreply.github.com>
2022-01-28 10:07:50 +0300

    Version 5
    Core deleted, because it's very very very BAD

---
+++ Onion/SourceCode/ast/lib/Stack/StackAccesExpression.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class StackAccesExpression : Expression
+    {
+        private string variable;
+        private Expression index;
+
+        public StackAccesExpression(string variable, Expression index)
+        {
+            this.variable = variable;
+            this.index = index;
+        }
+
+        public Value eval()
+        {
+            Value var = Variables.get(variable);
+            //if (var is ArrayValue)
+            try
+            {
+                StackValue array = (StackValue)var;
+                return array.get_by_index((int)index.eval().asNumber());
+            }
+            catch
+            {
+                throw new Exception("Array expected");
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format((variable + '{' + index + '}').ToString(), variable, index);
+        }
+    }
+}
