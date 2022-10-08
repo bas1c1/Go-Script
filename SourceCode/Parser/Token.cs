316216c8016f132ce341d276f5ee8a68a8592060
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-10-08 12:33:00 +0300

    Onion

---
+++ SourceCode/Parser/Token.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang
+{
+    class Token
+    {
+        private TokenType type;
+
+        private string text;
+
+        public Token(TokenType type, string text)
+        {
+            this.type = type;
+            this.text = text;
+        }
+
+        public TokenType getType()
+        {
+            return type;
+        }
+
+        public void setType(TokenType type)
+        {
+            this.type = type;
+        }
+
+        public string getText()
+        {
+            return text;
+        }
+
+        public void setText(string text)
+        {
+            this.text = text;
+        }
+
+        override public string ToString()
+        {
+            return type + " " + text;
+        }
+    }
+}
---
+++ SourceCode/ast/FunctionStatement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class FunctionStatement : Statement
+    {
+        private FunctionalExpression function;
+        public FunctionStatement(FunctionalExpression function)
+        {
+            this.function = function;
+        }
+
+        public void execute()
+        {
+            function.eval();
+        }
+
+        public override string ToString()
+        {
+            return function.ToString();
+        }
+    }
+}
