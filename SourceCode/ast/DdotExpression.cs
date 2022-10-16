0a8935ac0c967bea8b553b0ec8d9e70919f7870c
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-10-16 17:07:22 +0300

    Add files via upload

---
+++ SourceCode/ast/DdotExpression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class DdotExpression : Expression
+    {
+        private EnumValue value;
+        private StringValue var;
+
+        public DdotExpression(EnumValue value, StringValue var)
+        {
+            this.value = new EnumValue(value.getAll());
+            this.var = var;
+        }
+
+        public Value eval()
+        {
+            return value.get(var.asString());
+        }
+
+        override public string ToString()
+        {
+            return value.asString();
+        }
+    }
+}
