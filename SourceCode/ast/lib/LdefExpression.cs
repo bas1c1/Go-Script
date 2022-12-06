a4dd5e097354cc10bc45bb46fe1dd73958bf6de0
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-12-06 09:43:22 +0300

    added "ldef" expression

---
+++ SourceCode/ast/lib/LdefExpression.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class LdefExpression : Expression
+    {
+        private List<string> arguments;
+        private Statement body;
+
+        public LdefExpression()
+        {
+            arguments = new List<string>();
+        }
+
+        public LdefExpression(Statement body, List<string> arguments)
+        {
+            this.body = body;
+            this.arguments = arguments;
+        }
+
+        public void addArgument(string arg)
+        {
+            arguments.Add(arg);
+        }
+
+        public Value eval()
+        {
+            return new ObjectValue(new Ldef(arguments, body));
+        }
+
+        public override string ToString()
+        {
+            return "lfunc" + '(' + arguments.ToString() + ')';
+        }
+    }
+}
