53567c10df594da7c8457ad48a72d17c9ffdc4be
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-12-06 17:20:39 +0300

    added ":=" operator

---
+++ SourceCode/ast/DdotEqAssignmentStatement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class DdotEqAssignmentStatement : Statement
+    {
+        private EnumValue value;
+        private StringValue var;
+        private Value value1;
+
+        public DdotEqAssignmentStatement(EnumValue value, StringValue var, Value value1)
+        {
+            this.value = new EnumValue(value.getAll());
+            this.var = var;
+            this.value1 = value1;
+        }
+
+        public void execute()
+        {
+            value.set(var.asString(), value1);
+        }
+
+        override public string ToString()
+        {
+            return value.asString();
+        }
+    }
+}
