1e9ff51b83270ab170104a2408a7bc39ea0b6508
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-12-07 14:41:59 +0300

    added "inst" operator

--- SourceCode/ast/DdotEqAssignmentStatement.cs
+++ SourceCode/ast/DdotEqAssignmentStatement.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +8,21 @@ namespace OwnLang.ast.lib
 {
     public class DdotEqAssignmentStatement : Statement
     {
+        private string valuet;
         private EnumValue value;
         private StringValue var;
         private Value value1;
 
-        public DdotEqAssignmentStatement(EnumValue value, StringValue var, Value value1)
+        public DdotEqAssignmentStatement(string value, StringValue var, Value value1)
         {
-            this.value = new EnumValue(value.getAll());
+            valuet = value;
             this.var = var;
             this.value1 = value1;
         }
 
         public void execute()
         {
+            value = new EnumValue(((EnumValue)Variables.get(valuet)).getAll());
             value.set(var.asString(), value1);
         }
 
