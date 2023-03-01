d63cab755456c6af6b2b771a42228c60ee73d9d3
bas1c <68533770+bas1c1@users.noreply.github.com>
2023-03-01 17:38:21 +0300

    1.9.2 - just fix

---
+++ SourceCode/ast/lib/NanStatement.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public class NanStatement : Statement
+    {
+        public void execute()
+        {
+            
+        }
+    }
+}
