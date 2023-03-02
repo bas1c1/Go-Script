d8387ae147a93de760fb3752d700bfbe62780bfc
bas1c <68533770+bas1c1@users.noreply.github.com>
2023-03-02 22:06:38 +0300

    Added preprocessor

---
+++ SourceCode/ast/lib/Marks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnLang.ast.lib
+{
+    public static class Marks
+    {
+        public static Dictionary<string, Expression> marks = new Dictionary<string, Expression>();
+    }
+
+    public class EndIfException : System.Exception, Statement
+    {
+        public void execute()
+        {
+            
+        }
+    }
+}
